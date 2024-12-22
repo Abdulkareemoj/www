3b4dbe4a2f55c6d21ebb0c8cfa255f5306af19bb
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-12-22 12:05:21 +0100

    one more time

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
 import rehypeExternalLinks from 'rehype-external-links'
+import rehypeUnwrapImages from 'rehype-unwrap-images'
 import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
@@ -13,7 +13,7 @@ import deno from '@deno/astro-adapter'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://abdulkareemoj.deno.dev',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		// decapCmsOauth(),
@@ -25,8 +25,9 @@ export default defineConfig({
 		icon()
 	],
 	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		remarkPlugins: [remarkReadingTime],
 		rehypePlugins: [
+			rehypeUnwrapImages,
 			[
 				rehypeExternalLinks,
 				{
